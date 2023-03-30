3f817191c5cbeea78c7d17354f3a7fdde25ca8f6
André Hänsel <haensel@creations.de>
2023-03-30 23:52:54 +0200

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <a v-for="item in navigation">
+    <div><component :is="item.icon" /> {{ item.name }}</div>
+  </a>
+</template>
+
+<script setup lang="ts">
+  import { HomeIcon } from '@heroicons/vue/24/outline';
+  
+  let navigation = [
+    {icon: HomeIcon, name: 'Home'}
+  ];
+</script>
